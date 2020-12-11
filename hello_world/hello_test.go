b7b425bae4f2dafdf8b200ea8521f4c876905e39
Ana <anaalicefm18@gmail.com>
2020-12-10 22:47:03 -0300

    refactoring based on tests

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	response := Hello("Ana")
-	expected := "Hello Ana"
+	assetMessage := func(t *testing.T, result, expected string) {
+		t.Helper()
 
-	if response != expected {
-		t.Errorf("Response '%s', expected '%s'", response, expected)
+		if result != expected {
+			t.Errorf("Result %s, should %s", result, expected)
+		}
 	}
+
+	t.Run("Say hello to people", func(t *testing.T) {
+		result := Hello("Ana")
+		expected := "Hello Ana"
+
+		assetMessage(t, result, expected)
+	})
+
+	t.Run("Say 'Hello world' when an empty string is passed", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello World"
+
+		assetMessage(t, result, expected)
+	})
 }
